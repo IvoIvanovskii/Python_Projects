68de0c3c82c4115d9e189534cd976e372d8437b6
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-06-15 13:52:33 +0200

    Create SimpleCalculator.py

---
+++ Simple_Calculator/SimpleCalculator.py
@@ -0,0 +1,14 @@
+from operator import add, sub, mul, truediv
+def Calculator ():
+    ops = {"+": add, "-": sub, "*": mul, "/": truediv}
+
+    while True:
+        num1 = float(input("Enter a number: "))
+        op = input("Enter an operator (+, -, *, /): ")
+        num2 = float(input("Enter a number: "))
+        
+
+        
+        
+
+    
