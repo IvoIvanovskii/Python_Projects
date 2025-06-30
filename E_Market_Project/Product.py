14c016a050be3fbc4af861d88b860d396d251595
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-06-30 17:57:59 +0200

    Added product file and code

---
+++ E_Market_Project/Product.py
@@ -0,0 +1,39 @@
+from db import connect
+
+def addProduct():
+    db = connect()
+    cursor = db.cursor()
+    productName = input("Enter the name of the product you want to add: ")
+    name_lower = productName.lower()
+
+    cursor.execute("SELECT product_name FROM products WHERE LOWER(product_name) = %s ", (name_lower,))
+    if cursor.fetchone():
+     print ("This prodcut already exists.")
+    else:
+       try:
+          price = float(input("Enter the price of the product you just added: "))
+          cursor.execute("INSERT INTO products (product_name, price) VALUES (%s, %s)", (productName, price))
+          db.commit()
+          print (f"{productName} added successfully. ")
+       except ValueError:
+          print("Invalid price was added.")
+
+    cursor.close()
+    db.close()
+def view_products():
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT product_name, price FROM products")
+    results = cursor.fetchall()
+
+    if not results:
+        print("The store is out of stock.\n")
+    else:
+        print("Product List:\n")
+        for name, price in results:
+            print(f"{name}: ${price:.2f}")
+        print()
+
+    cursor.close()
+    db.close()
+
