b59bf961d82c7fdc36e912d994c7fb022a6bc324
IvoIvanovskii <ivo.ivanovski.04@gmail.com>
2025-07-03 15:34:17 +0200

    quantity added and changePrice function

--- E_Market_Project/EMarket.sql
+++ E_Market_Project/EMarket.sql
@@ -1,26 +1,27 @@
-CREATE DATABASE EMarket;
+CREATE DATABASE IF NOT EXISTS EMarket;
 USE EMarket;
 
-CREATE TABLE products(
+CREATE TABLE products (
     product_id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(100) UNIQUE,
-    price DECIMAL(10, 2)
-); 
+    price DECIMAL(10, 2),
+    quantity INT
+);
 
-CREATE TABLE customers(
+CREATE TABLE customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(50),
     last_name VARCHAR(50),
     email VARCHAR(100),
     address VARCHAR(300)
-); 
+);
 
-CREATE TABLE orders(
+CREATE TABLE orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     product_id INT,
     full_amount DECIMAL(10, 2),
     order_date DATE,
-    FOREIGN KEY(customer_id) REFERENCES customers(customer_id),
-    FOREIGN KEY(product_id) REFERENCES products(product_id)
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
 );
--- E_Market_Project/Product.py
+++ E_Market_Project/Product.py
@@ -8,12 +8,12 @@ def addProduct():
 
     cursor.execute("SELECT product_name FROM products WHERE LOWER(product_name) = %s ", (name_lower,))
     if cursor.fetchone():
-     print ("This prodcut already exists.")
+     print ("This product already exists.")
     else:
        try:
           price = float(input("Enter the price of the product you just added: "))
           quantity = int(input("Enter quantity for the product: "))
-          cursor.execute("INSERT INTO products (product_name, price) VALUES (%s, %s)", (productName, price))
+          cursor.execute("INSERT INTO products (product_name, price, quantity) VALUES (%s, %s, %s)", (productName, price, quantity))
           db.commit()
           print (f"{productName} added successfully. ")
        except ValueError:
@@ -25,17 +25,46 @@ def addProduct():
 def viewProducts():
     db = connect()
     cursor = db.cursor()
-    cursor.execute("SELECT product_name, price FROM products")
+    cursor.execute("SELECT product_name, price, quantity FROM products")
     results = cursor.fetchall()
 
     if not results:
         print("The store is out of stock.\n")
     else:
         print("Product List:\n")
-        for name, price in results:
-            print(f"{name}: ${price:.2f}")
+        for name, price, quantity in results:
+            print(f"{name}: ${price:.2f}. Quantity: {quantity}")
         print()
 
     cursor.close()
     db.close()
 
+def changePrice():
+    db = connect()
+    cursor = db.cursor()
+    productName = input("Enter the product name whose price you want to change. ")
+    nameLower = productName.lower
+
+    cursor.execute("SELECT product_name FROM products WHERE LOWER(product_name) = %s", (nameLower,))
+
+    if cursor.fetchone():
+       try:
+          newPrice = float(input("Enter the wanted price: "))
+          cursor.execute("UPDATE products SET price = %s WHERE LOWER(product_name) = %s", (newPrice, nameLower))
+
+          print(f"Price has been changed successfuly for {productName}.")
+
+       except ValueError:
+          print(f"Entered invalid price for {productName}.")
+    else:
+       print("Product not found")
+       
+    cursor.close()
+    db.close()
+    
+          
+
+          
+
+
+    
